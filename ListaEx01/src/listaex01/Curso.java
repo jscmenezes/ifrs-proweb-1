55b42c0d037825a2ff41becf422e10643437010c
Jeronimo Menezes <jscmenezes@gmail.com>
2019-03-25 21:54:25 -0300

    Final do exercicio 1

---
+++ ListaEx01/src/listaex01/Curso.java
@@ -0,0 +1,44 @@
+
+package aula01_ex01;
+
+public class Curso {
+    
+    private String nome;
+    private double duracao;
+    private Data data;
+    
+    public Curso(){};
+    
+    public Curso(String nome, double duracao, Data data){
+        this.nome = nome;
+        this.duracao = duracao;
+        this.data = data;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(double duracao) {
+        this.duracao = duracao;
+    }
+    
+    @Override
+    public String toString(){
+        return "Curso[nome=" + nome + ", duracao=" + this.duracao + ", data=[" + data.toString() + "]";
+    }
+    
+    public void imprime(){
+        
+    }
+    
+    
+}
