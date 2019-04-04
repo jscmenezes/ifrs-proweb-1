03f2b20a21091c3b1aeba19431703debb8f90eaf
Jeronimo Menezes <jscmenezes@gmail.com>
2019-04-04 20:48:48 -0300

    Aula 04

--- ListaEx01/src/listaex01/TesteMenu.java
+++ ListaEx01/src/listaex01/TesteMenu.java
@@ -8,13 +8,38 @@ public class TesteMenu {
     public static void main(String[] args){
         
         Scanner in = new Scanner(System.in);
+        Curso cursos[] = new Curso[100];
         int cont = 0;
         
         while(true){
             int opcao = montaMenu();
             switch(opcao){
                 case 1:
-                    System.out.println("\nVoce digitou " + opcao + "\n");
+                    System.out.println("\n\nCadastro de curso: ");
+                    System.out.println("\n");
+                    
+                    System.out.println("Nome: \n");
+                    String nome = in.nextLine();
+                    System.out.println("Duracao: \n");
+                    double duracao = in.nextDouble();
+                    System.out.println("Data: \n\n");
+                    System.out.println("dia: \n");
+                    int dia = in.nextInt();
+                    System.out.println("mês: \n");
+                    int mes = in.nextInt();
+                    System.out.println("ano: \n");
+                    int ano = in.nextInt();
+                    
+                    System.out.println("Cadastrando o curso " + nome + " ...\n");
+                    
+                    cursos[cont] = new Curso(nome, duracao, new Data(dia,mes,ano));
+                    
+                    System.out.println("Cadastramento realizado!\n");
+                    System.out.println("\n");
+                    System.out.println("Nome: " + nome + "\n");
+                    System.out.println("Duracao: "+ duracao + "\n");
+                    System.out.println("Data: " + dia + "/" + mes + "/" + ano + "\n");
+                    System.out.println("\n");
                     break;
                 
                 case 0:
